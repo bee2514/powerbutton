3aebeea4691daa92b76f0a028bf72cc6adf39ac4
Ben Basil Paul <84306065+bee2514@users.noreply.github.com>
2021-05-17 20:33:43 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,14 @@
+function green(){
+document.body.style.background="green"
+}
+
+function orange(){
+    document.body.style.background="orangered"
+
+
+}
+
+function grey(){
+    document.body.style.background="grey"
+
+}
